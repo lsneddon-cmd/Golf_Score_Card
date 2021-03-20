bb289dd81876bce40bf1325e3da143543fb34907
Lewis Sneddon <>
2021-03-20 18:21:34 +0000

    Started view

---
+++ app/src/main/java/io/lewiscodes/golfscorecard/MainActivity.java
@@ -0,0 +1,14 @@
+package io.lewiscodes.golfscorecard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
